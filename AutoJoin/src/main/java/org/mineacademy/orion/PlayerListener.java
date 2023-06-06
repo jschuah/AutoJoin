2fc3b5e93b92aa2cf31b798394d7379abe33c2a4
Sean J. Chuah <11226650+jschuah@users.noreply.github.com>
2023-06-06 11:57:49 -0700

    Update PlayerListener.java
    Allows for multiple worlds.

--- AutoJoin/src/main/java/org/mineacademy/orion/PlayerListener.java
+++ AutoJoin/src/main/java/org/mineacademy/orion/PlayerListener.java
@@ -11,6 +11,8 @@ import org.mineacademy.fo.Common;
 
 public class PlayerListener implements Listener {
 
+	private static final List<String> worlds = Arrays.asList("plai_one", "plai_two", "plai_three");  // replace with your world names
+
 	@EventHandler
 	public void onJoin(final PlayerJoinEvent event) {
 		final Player player = event.getPlayer();
@@ -18,12 +20,13 @@ public class PlayerListener implements Listener {
 		// Check if the player is in the predefined list
 		if (AutoJoin.predefinedNames.contains(player.getName())) {
 			AutoJoin.currentGroup.add(player);
-			Common.tellLater(2, player, "You've joined a group!");
+			Common.tellLater(2, player, "You've been automatically assigned a group!");
+			Common.tellLater(2, player, "Teleportation will commence once player limit reached.")
 
 			// When a group of 2 players is formed
 			if (AutoJoin.currentGroup.size() == AutoJoin.GROUP_SIZE) {
 
-				final World world = Bukkit.getWorld("world");  // Replace "world" with your specific world name
+				final World world = Bukkit.getWorld(worlds.get(AutoJoin.random.nextInt(worlds.size())));
 
 				// Generate random X and Z coordinates within a certain range
 				final int x = AutoJoin.random.nextInt(1000) - 500;  // Value will be between -500 and 500
