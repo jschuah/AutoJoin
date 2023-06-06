8126a3253fd1a4a4bed5383082829e7c4a72734b
Sean J. Chuah <11226650+jschuah@users.noreply.github.com>
2023-06-06 16:07:40 -0700

    Update AutoJoin.java

--- AutoJoin/src/main/java/org/mineacademy/orion/AutoJoin.java
+++ AutoJoin/src/main/java/org/mineacademy/orion/AutoJoin.java
@@ -1,13 +1,14 @@
-// AutoJoin class:
-
 package org.mineacademy.orion;
 
 import org.bukkit.entity.Player;
+import org.bukkit.Location;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class AutoJoin extends SimplePlugin {
@@ -15,18 +16,26 @@ public class AutoJoin extends SimplePlugin {
 	public static final int GROUP_SIZE = 2;
 	public static List<String> predefinedNames = new ArrayList<>();  // Fill this with predefined names
 	public static List<Player> currentGroup = new ArrayList<>();
+	public static Map<String, Location> respawnLocations = new HashMap<>();
 	public static Random random = new Random();
+
+	private static AutoJoin instance;  // This is the new line
 
 	@Override
 	protected void onPluginStart() {
-		getLogger().info("All works, captain!");
-		System.out.println("All works from system out, boss!");
-		Common.log("Hello!");
+		getLogger().info("AutoJoin starting...");
 
 		registerEvents(new PlayerListener());
 
-		predefinedNames.add("KingAsiimov");
-		predefinedNames.add("KingAsiimovX");
-		predefinedNames.add("ArigatoSashimi");
+		predefinedNames.add("PLAI_1");
+		predefinedNames.add("PLAI_2");
+		predefinedNames.add("PLAI_5");
+		predefinedNames.add("PLAI_0");
+
+		instance = this;  // Set the instance
+	}
+
+	public static AutoJoin getInstance() {
+		return instance;  // Getter for the instance
 	}
 }
