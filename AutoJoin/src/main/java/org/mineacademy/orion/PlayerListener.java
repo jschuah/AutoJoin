5e845c4f21faefbee3385d341f508c20a66104a4
Sean J. Chuah <11226650+jschuah@users.noreply.github.com>
2023-06-19 12:41:44 -0700

    Update PlayerListener.java

--- AutoJoin/src/main/java/org/mineacademy/orion/PlayerListener.java
+++ AutoJoin/src/main/java/org/mineacademy/orion/PlayerListener.java
@@ -26,6 +26,7 @@ import java.util.List;
 public class PlayerListener implements Listener {
 
 	private static final List<String> worlds = Arrays.asList("plai_one", "plai_two", "plai_three");  // replace with your world names
+	private static final List<String> skins = Arrays.asList("SkyTheKidRS", "Herobrine", "CaptainSparklez", "Notch");
 
 	@EventHandler
 	public void onJoin(final PlayerJoinEvent event) {
@@ -46,46 +47,41 @@ public class PlayerListener implements Listener {
 	}
 
 	private void teleportGroup() {
-		Bukkit.getScheduler().runTaskLater(AutoJoin.getInstance(), new Runnable() {
-			@Override
-			public void run() {
-				final World world = Bukkit.getWorld(worlds.get(AutoJoin.random.nextInt(worlds.size())));
+		Bukkit.getScheduler().runTaskLater(AutoJoin.getInstance(), () -> {
+			final World world = Bukkit.getWorld(worlds.get(AutoJoin.random.nextInt(worlds.size())));
 
-				int x = AutoJoin.random.nextInt(1000) - 500;
-				final int z = AutoJoin.random.nextInt(1000) - 500;
+			int x = AutoJoin.random.nextInt(1000) - 500;
+			final int z = AutoJoin.random.nextInt(1000) - 500;
 
-				for (final Player groupPlayer : AutoJoin.currentGroup) {
-					final int y = world.getHighestBlockYAt(x, z);
+			for (final Player groupPlayer : AutoJoin.currentGroup) {
+				final int y = world.getHighestBlockYAt(x, z);
 
-					final Location randomLocation = new Location(world, x, y, z);
+				final Location randomLocation = new Location(world, x, y, z);
 
-					groupPlayer.teleport(randomLocation);
-					groupPlayer.setBedSpawnLocation(randomLocation, true);
-					AutoJoin.respawnLocations.put(groupPlayer.getName(), randomLocation);
+				groupPlayer.teleport(randomLocation);
+				groupPlayer.setBedSpawnLocation(randomLocation, true);
+				AutoJoin.respawnLocations.put(groupPlayer.getName(), randomLocation);
 
-					groupPlayer.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1));
+				groupPlayer.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1));
 
-					groupPlayer.sendTitle("§6Type /Audio if you haven't!", getCurrentUTCTime(), 10, 70, 20);
+				groupPlayer.sendTitle("§6Type /Audio if you haven't!", getCurrentUTCTime(), 10, 70, 20);
 
+				groupPlayer.playSound(groupPlayer.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
 
-					groupPlayer.playSound(groupPlayer.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
+				String prefix = generateRandomAlphanumericString();
+				String suffix = generateRandomAlphanumericString();
+				String skin = skins.get(AutoJoin.random.nextInt(skins.size()));
 
-					String prefix = generateRandomAlphanumericString();
-					String suffix = generateRandomAlphanumericString();
+				Bukkit.getScheduler().runTask(AutoJoin.getInstance(), () -> {
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nte player " + groupPlayer.getName() + " prefix " + prefix);
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nte player " + groupPlayer.getName() + " suffix " + suffix);
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "skin set " + groupPlayer.getName() + " " + skin);
+				});
 
-					Bukkit.getScheduler().runTask(AutoJoin.getInstance(), new Runnable() {
-						@Override
-						public void run() {
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nte player " + groupPlayer.getName() + " prefix " + prefix);
-							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "nte player " + groupPlayer.getName() + " suffix " + suffix);
-						}
-					});
-
-					x += 1;
-				}
-
-				AutoJoin.currentGroup.clear();
+				x += 1;
 			}
+
+			AutoJoin.currentGroup.clear();
 		}, 20L);
 	}
 
@@ -105,12 +101,7 @@ public class PlayerListener implements Listener {
 		World world = player.getWorld();
 
 		if (worlds.contains(world.getName())) {
-			Bukkit.getScheduler().runTaskLater(AutoJoin.getInstance(), new Runnable() {
-				@Override
-				public void run() {
-					player.spigot().respawn();
-				}
-			}, 1L);
+			Bukkit.getScheduler().runTaskLater(AutoJoin.getInstance(), () -> player.spigot().respawn(), 1L);
 		}
 	}
 
